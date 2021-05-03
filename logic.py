2502dfb27d34207b89b370d4b535ae43f3f80cdc
Mark Fomin <78644136+nightadmin@users.noreply.github.com>
2021-05-03 11:35:04 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,29 +1,38 @@
-import asyncio, aiohttp, time, schedule
+import requests, time, schedule
+from random import choice
 
-USER_AIMSID = "XXX.XXXXXXXXXX.XXXXXXXXXX:XXXXXXXXX"
+USER_AIMSID = ["XXX.XXXXXXXXXX.XXXXXXXXXX:XXXXXXXXX"]
 
 chats = ["752738404"]
 replics = ["Привет! Подписывайся на наши каналы ниже!", "https://icq.im/na_group"]
-async def main():
-
-    async with aiohttp.ClientSession() as session:
-        for i in chats:
-            for j in replics:
-                API_URL = "https://u.icq.net/api/v53/wim/im/sendIM?aimsid={token}&t={chatId}&message={text}&mentions=&f=json".format(token=choice(USER_AIMSID), chatId=i, text=j)
-                async with session.get(API_URL) as response:
-                print("Отправка сообщения {1} в чат {2}".format(1=j, 2=i))
-                print("Статус:", response.status)
-                print("Content-type:", response.headers['content-type'])
-                html = await response.text()
-                print("Результат:", html)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
 def setup():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    for i in chats:
+        for j in replics:
+            API_URL = "https://u.icq.net/api/v53/wim/im/sendIM?aimsid={token}&t={chatId}&message={text}&mentions=&f=json".format(token=choice(USER_AIMSID), chatId=i, text=j)
+            r = requests.get(API_URL)
+            print("Запрос выполнен. Попытка отправки сообщения \"{m}\" в чат \"{c}\".".format(m=j, c=i))
+            print("Результат:")
+            print(r.json())
+
     
-schedule.every().day.at("19:00").do(setup)
+schedule.every().day.at("07:01").do(setup)
+schedule.every().day.at("08:02").do(setup)
+schedule.every().day.at("09:03").do(setup)
+schedule.every().day.at("10:04").do(setup)
+schedule.every().day.at("11:05").do(setup)
+schedule.every().day.at("12:06").do(setup)
+schedule.every().day.at("13:07").do(setup)
+schedule.every().day.at("14:08").do(setup)
+schedule.every().day.at("15:09").do(setup)
+schedule.every().day.at("16:10").do(setup)
+schedule.every().day.at("17:11").do(setup)
+schedule.every().day.at("18:12").do(setup)
+schedule.every().day.at("19:13").do(setup)
+schedule.every().day.at("20:14").do(setup)
+schedule.every().day.at("21:15").do(setup)
+schedule.every().day.at("22:03").do(setup)
+schedule.every().day.at("23:17").do(setup)
+
 while True:
     schedule.run_pending()
     time.sleep(1)
